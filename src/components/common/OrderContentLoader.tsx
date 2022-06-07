fcfac5407db7f4b6d23ec833764acab426446c38
magnetoasher <magnetoasher@gmail.com>
2022-06-07 23:00:34 +0100

    refactor: update content loader colors

--- src/components/common/OrderContentLoader.tsx
+++ src/components/common/OrderContentLoader.tsx
@@ -1,15 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import ContentLoader from "react-content-loader";
 
 const OrderContentLoader = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <Box>
       <ContentLoader
         height={460}
         width={"100%"}
         speed={0.9}
-        backgroundColor={"#0F3460"}
-        foregroundColor={"#999"}
+        backgroundColor={colorMode === "light" ? "#d9d9d9" : "#333"}
+        foregroundColor={colorMode === "light" ? "#ededed" : "#999"}
       >
         <rect rx="6" ry="6" width={110} height={35} />
         <rect y="59" rx="6" ry="6" width={"100%"} height={401} />
--- src/components/hero/ImageLoader.tsx
+++ src/components/hero/ImageLoader.tsx
@@ -1,15 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import ContentLoader from "react-content-loader";
 
 const ImageLoader = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <Box position="absolute" top="0" bottom="0" left="0" right="0">
       <ContentLoader
         height="100%"
         width="100%"
         speed={0.9}
-        backgroundColor={"#0F3460"}
-        foregroundColor={"#999"}
+        backgroundColor={colorMode === "light" ? "#d9d9d9" : "#333"}
+        foregroundColor={colorMode === "light" ? "#ededed" : "#999"}
         viewBox="0 0 100% 100%"
       >
         <rect rx="6" ry="6" width="100%" height="100%" />
